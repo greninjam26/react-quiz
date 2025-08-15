ee9672ac9a7d01ad3919c99e4f85aa5726d8ef7b
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-14 21:40:10 -0400

    answering the question

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ const initialState = {
 		// "loading", "error", "ready", "active", "finished"
 		"loading",
 	index: 0,
+	answer: null,
+	points: 0,
 };
 
 function reducer(state, action) {
@@ -23,13 +25,26 @@ function reducer(state, action) {
 			return { ...state, status: "error" };
 		case "start":
 			return { ...state, status: "active" };
+		case "newAnswer":
+			const question = state.questions.at(state.index);
+			return {
+				...state,
+				answer: action.payLoad,
+				point:
+					action.payLoad === question.correctOption
+						? state.points + question.points
+						: state.points,
+			};
 		default:
 			throw new Error("Action Unknown");
 	}
 }
 
 export default function App() {
-	const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+		reducer,
+		initialState
+	);
 
 	const numQuestions = questions.length;
 
@@ -49,7 +64,13 @@ export default function App() {
 				{status === "ready" && (
 					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
 				)}
-				{status === "active" && <Questions question={questions[index]} />}
+				{status === "active" && (
+					<Questions
+						question={questions[index]}
+						dispatch={dispatch}
+						answer={answer}
+					/>
+				)}
 			</Main>
 		</div>
 	);
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,21 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+	const hasAnswered = answer !== null;
+
 	return (
 		<div className="options">
-			{question.options.map(option => (
-				<button className="btn btn-option" key={option}>
+			{question.options.map((option, i) => (
+				<button
+					className={`btn btn-option ${i === answer ? "answer" : ""} ${
+						hasAnswered
+							? i === question.correctOption
+								? "correct"
+								: "wrong"
+							: ""
+					}`}
+					key={option}
+					disabled={hasAnswered}
+					onClick={() => dispatch({ type: "newAnswer", payLoad: i })}
+				>
 					{option}
 				</button>
 			))}

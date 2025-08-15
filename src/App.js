4ede2e7750287b9865c858952ac8f349906192c1
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-14 20:52:57 -0400

    json-server for API, and process the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+	questions: [],
+	status:
+		// "loading", "error", "ready", "active", "finished"
+		"loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataRecived":
+			return { ...state, questions: action.payLoad, status: "ready" };
+		case "dataFailed":
+			return { ...state, status: error };
+		default:
+			throw new Error("Action Unknown");
+	}
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(function () {
+		fetch("http://localhost:8000/questions")
+			.then(resp => resp.json())
+			.then(data => dispatch({ type: "dataRecived", payLoad: data }))
+			.catch(err => dispatch({ type: "dataFailed" }));
+	}, []);
+
 	return (
 		<div className="app">
-			<DateCounter />
+			<Header />
+			<Main>
+				<p>1/15</p>
+				<p>Questions?</p>
+			</Main>
 		</div>
 	);
 }
